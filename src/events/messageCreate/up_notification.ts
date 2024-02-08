55ad2380aa28521d7f0291f4ca9b1f2ddadc5ec7
Kotonekotokoto <y.games@outlook.jp>
2024-02-08 18:34:44 +0900

    Fix: ディス速の通知が正常に行われない問題を修正#1

--- src/events/messageCreate/up_notification.ts
+++ src/events/messageCreate/up_notification.ts
@@ -2,8 +2,8 @@ import { Event } from '../../lib/modules/Event';
 import { Colors } from 'discord.js';
 import { footer } from '../../lib/utils/Embed';
 
-export default new Event('messageUpdate', async (message) => {
-  if (message.author?.id === '761562078095867916') {
+export default new Event('messageCreate', async (message) => {
+  if (message.author.id === '761562078095867916') {
     if (
       message.embeds[0]?.fields[0]?.name.match(/をアップしたよ/) ||
       message.embeds[0]?.fields[0]?.name.match(/I've bumped up/)
