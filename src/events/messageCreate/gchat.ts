ab426826603943f553e1861b606a65d1835d1b5c
Kotonekotokoto <y.games@outlook.jp>
2024-02-09 22:40:09 +0900

    Fix: 画像等の送受信に対応

--- src/events/messageCreate/gchat.ts
+++ src/events/messageCreate/gchat.ts
@@ -1,10 +1,12 @@
 import { Event } from '../../lib/modules/Event';
 import { gchat_model } from '../../lib/models/gchat';
-import { Colors, WebhookClient } from 'discord.js';
+import { AttachmentBuilder, Colors, WebhookClient } from 'discord.js';
 import { client } from '../../index';
 
 export default new Event('messageCreate', async (message) => {
   if (message.author.bot || !message.guild || !message.channel) return;
+
+  if (!message.content && !message.attachments) return;
 
   const data = await gchat_model.findOne({
     ChannelID: message.channel.id,
@@ -44,6 +46,9 @@ export default new Event('messageCreate', async (message) => {
         allowedMentions: {
           parse: [],
         },
+        files: message.attachments.map(
+          (attachment) => new AttachmentBuilder(attachment.proxyURL)
+        ),
       });
     } catch (e) {
       return e;
