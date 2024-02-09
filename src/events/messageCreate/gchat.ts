55ed8a53320d56d26536df6704e75b8c20f3a216
Kotonekotokoto <y.games@outlook.jp>
2024-02-09 21:54:40 +0900

    Add: Gchatの送受信を追加

---
+++ src/events/messageCreate/gchat.ts
@@ -0,0 +1,55 @@
+import { Event } from '../../lib/modules/Event';
+import { gchat_model } from '../../lib/models/gchat';
+import { Colors, WebhookClient } from 'discord.js';
+import { client } from '../../index';
+
+export default new Event('messageCreate', async (message) => {
+  if (message.author.bot || !message.guild || !message.channel) return;
+
+  const data = await gchat_model.findOne({
+    ChannelID: message.channel.id,
+  });
+
+  if (!data) return;
+
+  const global_chats = await gchat_model.find();
+
+  global_chats.forEach((data) => {
+    if (!data || !data.Webhook) {
+      return gchat_model.findOneAndDelete({
+        GuildID: data.GuildID,
+        ChannelID: data.ChannelID,
+      });
+    }
+
+    if (data.ChannelID === message.channel?.id) return;
+
+    const channel = client.channels.cache.get(data.ChannelID);
+    if (!channel) {
+      return gchat_model.deleteMany({
+        GuildID: data.GuildID,
+        ChannelID: data.ChannelID,
+      });
+    }
+
+    const hook = new WebhookClient({
+      url: data.Webhook.URL,
+    });
+
+    try {
+      hook.send({
+        content: message.content,
+        username: `${message.author.displayName}(${message.author.tag})`,
+        avatarURL: message.author.displayAvatarURL(),
+        allowedMentions: {
+          parse: [],
+        },
+      });
+    } catch (e) {
+      gchat_model.findOneAndDelete({
+        GuildID: data.GuildID,
+        ChannelID: data.ChannelID,
+      });
+    }
+  });
+});
