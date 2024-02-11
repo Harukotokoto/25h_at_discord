194deb5d1d4ac1a96af98fceaafa010eedb6c0ce
Harukotokoto <n_rui@outlook.jp>
2024-02-11 23:53:22 +0900

    Fix: Helpの修正

--- src/commands/information/help.ts
+++ src/commands/information/help.ts
@@ -8,126 +8,17 @@ import {
 } from 'discord.js';
 import { footer } from '../../lib/utils/Embed';
 
-const commands = getCommands();
-
-const categories = [...new Set(commands.map((command) => command.category))];
-
-const menuActions = async (i: StringSelectMenuInteraction) => {
-  if (i.customId === 'category_select') {
-    const selectedCategory = i.values[0];
-
-    const categoryName =
-      selectedCategory.charAt(0).toUpperCase() + selectedCategory.slice(1);
-
-    const commandList = commands.filter(
-      (cmd) => cmd.category === selectedCategory
-    );
-
-    await i.update({
-      embeds: [
-        {
-          title: categoryName,
-          fields: commandList.map((command) => {
-            return {
-              name: command.command.name,
-              value: command.command.description,
-              inline: true,
-            };
-          }),
-          color: Colors.Gold,
-          footer: footer(),
-        },
-      ],
-      components: [
-        new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(
-          new StringSelectMenuBuilder()
-            .setOptions(
-              categories.map((category) => {
-                const categoryName =
-                  category.charAt(0).toUpperCase() + category.slice(1);
-
-                return {
-                  label: categoryName,
-                  value: category,
-                };
-              })
-            )
-            .setPlaceholder('カテゴリーを選択')
-            .setCustomId('category_select')
-        ),
-        new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(
-          new StringSelectMenuBuilder()
-            .setCustomId('command_select')
-            .setOptions(
-              commands
-                .filter((command) => command.category === selectedCategory)
-                .map((command) => {
-                  return {
-                    label: '/' + command.command.name,
-                    value: command.command.name,
-                    description: command.command.description,
-                  };
-                })
-            )
-            .setPlaceholder('コマンドを選択')
-        ),
-      ],
-    });
-  }
-
-  if (i.customId === 'command_select') {
-    const selectedCommand = i.values[0];
-    const cmd = commands.find((cmd) => cmd.command.name === selectedCommand);
-    const command = cmd?.command;
-
-    if (!command) return;
-
-    const commandName =
-      command.name.charAt(0).toUpperCase() + command.name.slice(1);
-    const aliases = command.aliases
-      .map((alias: string) => '`' + alias + '`')
-      .join(', ');
-    const commandDescription = command.description;
-    const commandUsage = command.usage;
-    const isOwnerCommand = command.isOwnerCommand;
-    const categoryName =
-      cmd?.category.charAt(0).toUpperCase() + cmd?.category.slice(1);
-
-    i.update({
-      embeds: [
-        {
-          title: cmd?.category
-            ? categoryName + ' - ' + commandName
-            : commandName,
-          description:
-            commandDescription +
-            '\n' +
-            (isOwnerCommand
-              ? '```fix\n※このコマンドは管理者限定です\n```'
-              : ''),
-          fields: [
-            {
-              name: 'エイリアス',
-              value: aliases || 'このコマンドにエイリアスは設定されていません',
-            },
-            {
-              name: '使用方法',
-              value: `\`\`\`\n${commandUsage}\n\`\`\``,
-            },
-          ],
-          color: Colors.Gold,
-          footer: footer(),
-        },
-      ],
-    });
-  }
-};
-
 export default new Command({
   name: 'help',
   description: 'コマンドの使用方法を表示します',
   execute: {
     interaction: async ({ interaction }) => {
+      const commands = await getCommands();
+
+      const categories = [
+        ...new Set(commands?.map((command) => command.category)),
+      ];
+
       await interaction.followUp({
         embeds: [
           {
@@ -178,10 +69,129 @@ export default new Command({
       collecter.on('collect', async (i) => {
         if (!i.isStringSelectMenu()) return;
 
-        await menuActions(i);
+        if (i.customId === 'category_select') {
+          const selectedCategory = i.values[0];
+
+          const categoryName =
+            selectedCategory.charAt(0).toUpperCase() +
+            selectedCategory.slice(1);
+
+          const commandList = commands?.filter(
+            (cmd) => cmd.category === selectedCategory
+          );
+
+          await i.update({
+            embeds: [
+              {
+                title: categoryName,
+                fields: commandList?.map((command) => {
+                  return {
+                    name: command.command.name,
+                    value: command.command.description,
+                    inline: true,
+                  };
+                }),
+                color: Colors.Gold,
+                footer: footer(),
+              },
+            ],
+            components: [
+              new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(
+                new StringSelectMenuBuilder()
+                  .setOptions(
+                    categories.map((category) => {
+                      const categoryName =
+                        category.charAt(0).toUpperCase() + category.slice(1);
+
+                      return {
+                        label: categoryName,
+                        value: category,
+                      };
+                    })
+                  )
+                  .setPlaceholder('カテゴリーを選択')
+                  .setCustomId('category_select')
+              ),
+              new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(
+                new StringSelectMenuBuilder()
+                  .setCustomId('command_select')
+                  .setOptions(
+                    commands
+                      .filter(
+                        (command) => command.category === selectedCategory
+                      )
+                      .map((command) => {
+                        return {
+                          label: '/' + command.command.name,
+                          value: command.command.name,
+                          description: command.command.description,
+                        };
+                      })
+                  )
+                  .setPlaceholder('コマンドを選択')
+              ),
+            ],
+          });
+        }
+
+        if (i.customId === 'command_select') {
+          const selectedCommand = i.values[0];
+          const cmd = commands?.find(
+            (cmd) => cmd.command.name === selectedCommand
+          );
+          const command = cmd?.command;
+
+          if (!command) return;
+
+          const commandName =
+            command.name.charAt(0).toUpperCase() + command.name.slice(1);
+          const aliases = command.aliases
+            .map((alias: string) => '`' + alias + '`')
+            .join(', ');
+          const commandDescription = command.description;
+          const commandUsage = command.usage;
+          const isOwnerCommand = command.isOwnerCommand;
+          const categoryName =
+            cmd?.category.charAt(0).toUpperCase() + cmd?.category.slice(1);
+
+          i.update({
+            embeds: [
+              {
+                title: cmd?.category
+                  ? categoryName + ' - ' + commandName
+                  : commandName,
+                description:
+                  commandDescription +
+                  '\n' +
+                  (isOwnerCommand
+                    ? '```fix\n※このコマンドは管理者限定です\n```'
+                    : ''),
+                fields: [
+                  {
+                    name: 'エイリアス',
+                    value:
+                      aliases || 'このコマンドにエイリアスは設定されていません',
+                  },
+                  {
+                    name: '使用方法',
+                    value: `\`\`\`\n${commandUsage}\n\`\`\``,
+                  },
+                ],
+                color: Colors.Gold,
+                footer: footer(),
+              },
+            ],
+          });
+        }
       });
     },
     message: async ({ client, message, args }) => {
+      const commands = await getCommands();
+
+      const categories = [
+        ...new Set(commands?.map((command) => command.category)),
+      ];
+
       const msg = await message.reply({
         embeds: [
           {
@@ -233,7 +243,120 @@ export default new Command({
       collecter.on('collect', async (i) => {
         if (!i.isStringSelectMenu()) return;
 
-        await menuActions(i);
+        if (i.customId === 'category_select') {
+          const selectedCategory = i.values[0];
+
+          const categoryName =
+            selectedCategory.charAt(0).toUpperCase() +
+            selectedCategory.slice(1);
+
+          const commandList = commands.filter(
+            (cmd) => cmd.category === selectedCategory
+          );
+
+          await i.update({
+            embeds: [
+              {
+                title: categoryName,
+                fields: commandList.map((command) => {
+                  return {
+                    name: command.command.name,
+                    value: command.command.description,
+                    inline: true,
+                  };
+                }),
+                color: Colors.Gold,
+                footer: footer(),
+              },
+            ],
+            components: [
+              new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(
+                new StringSelectMenuBuilder()
+                  .setOptions(
+                    categories.map((category) => {
+                      const categoryName =
+                        category.charAt(0).toUpperCase() + category.slice(1);
+
+                      return {
+                        label: categoryName,
+                        value: category,
+                      };
+                    })
+                  )
+                  .setPlaceholder('カテゴリーを選択')
+                  .setCustomId('category_select')
+              ),
+              new ActionRowBuilder<StringSelectMenuBuilder>().addComponents(
+                new StringSelectMenuBuilder()
+                  .setCustomId('command_select')
+                  .setOptions(
+                    commands
+                      .filter(
+                        (command) => command.category === selectedCategory
+                      )
+                      .map((command) => {
+                        return {
+                          label: '/' + command.command.name,
+                          value: command.command.name,
+                          description: command.command.description,
+                        };
+                      })
+                  )
+                  .setPlaceholder('コマンドを選択')
+              ),
+            ],
+          });
+        }
+
+        if (i.customId === 'command_select') {
+          const selectedCommand = i.values[0];
+          const cmd = commands.find(
+            (cmd) => cmd.command.name === selectedCommand
+          );
+          const command = cmd?.command;
+
+          if (!command) return;
+
+          const commandName =
+            command.name.charAt(0).toUpperCase() + command.name.slice(1);
+          const aliases = command.aliases
+            .map((alias: string) => '`' + alias + '`')
+            .join(', ');
+          const commandDescription = command.description;
+          const commandUsage = command.usage;
+          const isOwnerCommand = command.isOwnerCommand;
+          const categoryName =
+            cmd?.category.charAt(0).toUpperCase() + cmd?.category.slice(1);
+
+          i.update({
+            embeds: [
+              {
+                title: cmd?.category
+                  ? categoryName + ' - ' + commandName
+                  : commandName,
+                description:
+                  commandDescription +
+                  '\n' +
+                  (isOwnerCommand
+                    ? '```fix\n※このコマンドは管理者限定です\n```'
+                    : ''),
+                fields: [
+                  {
+                    name: 'エイリアス',
+                    value:
+                      aliases || 'このコマンドにエイリアスは設定されていません',
+                  },
+                  {
+                    name: '使用方法',
+                    value: `\`\`\`\n${commandUsage}\n\`\`\``,
+                  },
+                ],
+                color: Colors.Gold,
+                footer: footer(),
+              },
+            ],
+          });
+        }
       });
     },
   },
