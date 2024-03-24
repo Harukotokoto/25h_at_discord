ce31dcb55be2a55387234bcd4042b11fb9daa42b
Harukotokoto <n_rui@outlook.jp>
2024-03-25 00:00:22 +0900

    レベリングが正しく機能しない問題を修正

--- src/events/messageCreate/leveling.ts
+++ src/events/messageCreate/leveling.ts
@@ -5,17 +5,17 @@ import { leveling_model } from '../../lib/models/config';
 export default new Event('messageCreate', async (message) => {
   if (!message.guild) return;
 
-  const leveling = leveling_model.findOne({
+  const leveling = await leveling_model.findOne({
     GuildID: message.guild.id,
   });
 
-  if (!leveling) return;
+  if (leveling) {
+    if (!message.guild || message.author.bot) return;
+    const member = message.guild.members.cache.get(message.author.id);
+    if (!member) return;
 
-  if (!message.guild || message.author.bot) return;
-  const member = message.guild.members.cache.get(message.author.id);
-  if (!member) return;
+    const levelingManager = new Leveling(member);
 
-  const levelingManager = new Leveling(member);
-
-  await levelingManager.addExperience();
+    await levelingManager.addExperience();
+  }
 });
