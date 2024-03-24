dad1051bb2b087114190d3bf03a60499b0259a2e
Harukotokoto <n_rui@outlook.jp>
2024-03-24 23:13:13 +0900

    レベリングの切り替えを実装

--- src/events/messageCreate/leveling.ts
+++ src/events/messageCreate/leveling.ts
@@ -1,7 +1,16 @@
 import { Event } from '../../lib/modules/Event';
 import { Leveling } from '../../lib/modules/classes/Leveling';
+import { leveling_model } from '../../lib/models/config';
 
 export default new Event('messageCreate', async (message) => {
+  if (!message.guild) return;
+
+  const leveling = leveling_model.findOne({
+    GuildID: message.guild.id,
+  });
+
+  if (!leveling) return;
+
   if (!message.guild || message.author.bot) return;
   const member = message.guild.members.cache.get(message.author.id);
   if (!member) return;
