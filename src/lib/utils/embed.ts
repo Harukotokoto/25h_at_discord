04a0a240d187e550bd944161e671fac3cf55fab1
Harukotokoto <n_rui@outlook.jp>
2024-03-31 23:46:48 +0900

    フッターを変更

--- src/lib/utils/embed.ts
+++ src/lib/utils/embed.ts
@@ -32,7 +32,7 @@ import { createBar } from './createBar';
 const footer = (): APIEmbedFooter => {
   const user = client.users.cache.get('1004365048887660655');
   return {
-    text: `Produced by Rena`,
+    text: `Developed by はること`,
     icon_url: user?.avatarURL() as string,
   };
 };
