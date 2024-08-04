79c3251074bd2a22837a0074df3d7eed6c615107
Harukotokoto <n_rui@outlook.jp>
2024-08-05 00:05:53 +0900

    ephemeralを有効化

--- src/commands/economy/gift.ts
+++ src/commands/economy/gift.ts
@@ -9,6 +9,7 @@ import { footer } from '../../lib/utils/embed';
 export default new Command({
   name: 'gift',
   description: 'ギフトに関するコマンド',
+  ephemeral: true,
   options: [
     {
       name: 'create',
@@ -85,7 +86,6 @@ export default new Command({
                 footer: footer(),
               },
             ],
-            ephemeral: true,
           });
           break;
         case 'use':
